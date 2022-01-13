12285fb31f651f0fe7b3887e2ed93ffb5bdcf887
Eduardo Arcila <eduardocila11@gmail.com>
2022-01-12 23:10:11 -0300

    Arreglando variables

--- html/js/footer.js
+++ html/js/footer.js
@@ -1,16 +1,8 @@
-let footer = [
-  {
-    iconos: "fab fa-facebook-square",
-  },
-];
-
-footer.forEach(() => {
-  $("footer").append(`
+let footer = $("footer").append(`
       <div class="d-flex flex-wrap contenido align-items-center py-3 margin bg-dark"> <p class="col-md-4 mb-0 text-muted2 centradoNombre powered">&copy; Powered by Eduardo Arcila</p>
-     
+
     <a href="https://www.facebook.com/" target="_blank"> <i class="fab fa-facebook-square color__1"></i></a> 
     <a href="https://web.whatsapp.com/" target="_blank"> <i class="fab fa-whatsapp color__2"></i></a>
     <a href="https://www.instagram.com/?hl=es" target="_blank"> <i class="fab fa-instagram color__3"></i></a>
     <a href="https://twitter.com/?lang=es" target="_blank"><i class="fab fa-twitter color__4"></i> 
-   </div> `);
-});
+  </div> `);
--- html/js/navbar.js
+++ html/js/navbar.js
@@ -1,13 +1,5 @@
-let navBar = [
-  {
-    logo: "./styles/imagenesJs/telefono-inteligente.png",
-    logoCarrito: "fas fa-shopping-cart",
-  },
-];
-
-navBar.forEach((navBar1) => {
-  $("#navbar").append(` <div class="navbar bg-dark navbar-dark container-fluid">
-          <a  class="navbar-brand" href="index.html"><img src= ${navBar1.logo}>Tecnitel</a>
-      <i class="fas fa-shopping-cart ${navBar1.logoCarrito}"></i>
+let navbar1 = $("#navbar")
+  .append(` <div class="navbar bg-dark navbar-dark container-fluid">
+          <a  class="navbar-brand" href="index.html"><img src= "./styles/imagenesJs/telefono-inteligente.png" >Tecnitel</a>
+          <button class= "btn btn-dark addToCart"> Contactanos! </button>
   </div>`);
-});
