90cdf36401f5718ce432454668af9522364bcc5a
Eduardo Arcila <eduardocila11@gmail.com>
2022-01-17 20:53:15 -0300

    Modificando estilos

--- html/js/carrusel.js
+++ html/js/carrusel.js
@@ -1,46 +1,83 @@
-let imagenesCarrusel = [
-  "./styles/imagenesJs/imagenCarrusel3.jpg",
-  "./styles/imagenesJs/imagenCarrusel2.png",
-  "./styles/imagenesJs/imagenCarrusel1.jpg",
-];
+// let imagenesCarrusel = [
+//   "./styles/imagenesJs/imagenCarrusel3.jpg",
+//   "./styles/imagenesJs/imagenCarrusel2.png",
+//   "./styles/imagenesJs/imagenCarrusel1.jpg",
+// ];
 
-let contador = 0;
+// let contador = 0;
 
-function carrousel(direccion) {
-  let direccion1 = direccion;
+// function carrousel(direccion) {
+//   let direccion1 = direccion;
 
-  if (direccion1 == "atras") {
-    if (contador == 0) {
-      document.getElementById("imagen").src =
-        imagenesCarrusel[imagenesCarrusel.length - 1];
+//   if (direccion1 == "atras") {
+//     if (contador == 0) {
+//       document.getElementById("imagen").src =
+//         imagenesCarrusel[imagenesCarrusel.length - 1];
 
-      contador++;
-    } else if (contador < imagenesCarrusel.length - 1) {
-      document.getElementById("imagen").src =
-        imagenesCarrusel[imagenesCarrusel.length - 1 - contador];
+//       contador++;
+//     } else if (contador < imagenesCarrusel.length - 1) {
+//       document.getElementById("imagen").src =
+//         imagenesCarrusel[imagenesCarrusel.length - 1 - contador];
 
-      contador++;
-    } else {
-      document.getElementById("imagen").src = imagenesCarrusel[0];
+//       contador++;
+//     } else {
+//       document.getElementById("imagen").src = imagenesCarrusel[0];
 
-      contador = 0;
-    }
-  }
+//       contador = 0;
+//     }
+//   }
 
-  if (direccion1 == "adelante") {
-    if (contador == 0) {
-      document.getElementById("imagen").src = imagenesCarrusel[0];
+//   if (direccion1 == "adelante") {
+//     if (contador == 0) {
+//       document.getElementById("imagen").src = imagenesCarrusel[0];
 
-      contador++;
-    } else if (contador < imagenesCarrusel.length - 1) {
-      document.getElementById("imagen").src = imagenesCarrusel[contador];
+//       contador++;
+//     } else if (contador < imagenesCarrusel.length - 1) {
+//       document.getElementById("imagen").src = imagenesCarrusel[contador];
 
-      contador++;
-    } else {
-      document.getElementById("imagen").src =
-        imagenesCarrusel[imagenesCarrusel.length - 1];
+//       contador++;
+//     } else {
+//       document.getElementById("imagen").src =
+//         imagenesCarrusel[imagenesCarrusel.length - 1];
 
-      contador = 0;
-    }
-  }
-}
+//       contador = 0;
+//     }
+//   }
+// }
+
+let carru = $("#contenedorCarrusel").append(`</div>
+<div class="sec">
+  <div id="demo" class="carousel slide" data-ride="carousel">
+
+    <!-- Indicators -->
+    <ul class="carousel-indicators">
+      <li data-target="#demo" data-slide-to="0" class="active"></li>
+      <li data-target="#demo" data-slide-to="1"></li>
+      <li data-target="#demo" data-slide-to="2"></li>
+     
+    </ul>
+  
+    <!-- The slideshow -->
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="styles/imagenesJs/imagenCarrusel1.jpg" alt="Imagen de Granos de Cafe">
+        
+      </div>
+      <div class="carousel-item">
+        <img src="styles/imagenesJs/imagenCarrusel2.png" alt="Imagen de cafe tostado">
+      </div>
+      <div class="carousel-item">
+        <img src="styles/imagenesJs/imagenCarrusel3.jpg" alt="Imagen de cafeteria">
+      </div>
+    </div>
+  
+    <!-- Left and right controls -->
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </a>
+  
+  </div>
+</div>`);
--- html/js/navbar.js
+++ html/js/navbar.js
@@ -1,61 +1,9 @@
 let navbar1 = $("#navbar")
   .append(` <div class="navbar colorNav navbar-dark container-fluid">
           <a  class="navbar-brand logo" href="index.html"><img src= "./styles/imagenesJs/telefono-inteligente.png" >Tecnitel</a>
-         <a href="#formulario"  class= "btn btn-dark addToCart"> Contactanos! </a>
+         <a href="#formulario"  type="button" class="btn btn-secondary">Contactanos!</a>
   </div>`);
 
-let formu = $("#formulario").append(`<div class="container">
-<div class="row ">
-    <div class="col-md-12 ">
-        <div class="well well-sm">
-            <form action="https://formsubmit.co/eduardocila11@gmail.com" class="form-horizontal" method="post">
-                <fieldset>
-                    <legend class="text-center header fondoContacto ">  
-                      ¿Quieres saber más de nosotros? ¡Contáctanos! 
-                      </legend>
-                  
-                    <div class="form-group">
-                        <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
-                        <div class="col-md-8">
-                            <input id="fname" name="name" type="text" placeholder="Nombre" aria-label="nombre" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
-                        <div class="col-md-8">
-                            <input id="lname" name="name" type="text" placeholder="Apellido" class="form-control">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <span class="col-md-1 col-md-offset-2 text-center"><i class="fas fa-envelope"></i></i></span>
-                        <div class="col-md-8">
-                            <input id="email" name="email" type="text" placeholder="Correo Electronico" class="form-control">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
-                        <div class="col-md-8">
-                            <input id="phone" name="phone" type="text" placeholder="Numero de Telefono" class="form-control">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
-                        <div class="col-md-8">
-                            <textarea class="form-control" id="message" name="message" placeholder="Envianos tu mensaje, nos contactaremos contigo lo mas pronto posible!" rows="7"></textarea>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <div class="col-md-12 text-center">
-                            <button type="submit" class="btn btn-primary btn-lg">Enviar</button>
-                        </div>
-                    </div>
-                </fieldset>
-            </form>
-        </div>
-    </div>
-</div>
-</div>`);
+let BarraTelefono = $("#modal").append(
+  `<h3> Para ventas telefonicas 0800 2322 4424 </h3>`
+);
