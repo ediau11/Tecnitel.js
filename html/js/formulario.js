f4f7653aff4bc1a20a349cf6f683f5cef86a5dbe
Eduardo Arcila <eduardocila11@gmail.com>
2022-01-19 23:50:47 -0300

    agregando estilos y dandole funcionalidad a mi formulario

--- html/js/formulario.js
+++ html/js/formulario.js
@@ -2,22 +2,22 @@ let formu = $("#formulario").append(`<div class="container">
 <div class="row ">
     <div class="col-md-12 ">
         <div class="well well-sm">
-            <form action="https://formsubmit.co/eduardocila11@gmail.com" class="form-horizontal" method="post">
+            <form>
                 <fieldset>
                     <legend class="text-center header fondoContacto ">  
-                      ¿Quieres saber más de nosotros? ¡Contáctanos! 
-                      </legend>
-                  
+                ¿Quieres saber más de nosotros? ¡Contáctanos! 
+                    </legend>
+                
                     <div class="form-group">
                         <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
                         <div class="col-md-8">
-                            <input id="fname" name="name" type="text" placeholder="Nombre" aria-label="nombre" class="form-control">
+                            <input id="nombreFormulario" name="name" type="text" placeholder="Nombre" aria-label="nombre" class="form-control">
                         </div>
                     </div>
                     <div class="form-group">
                         <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
                         <div class="col-md-8">
-                            <input id="lname" name="name" type="text" placeholder="Apellido" class="form-control">
+                            <input id="apellidoFormulario" name="name" type="text" placeholder="Apellido" class="form-control">
                         </div>
                     </div>
 
@@ -44,7 +44,7 @@ let formu = $("#formulario").append(`<div class="container">
 
                     <div class="form-group">
                         <div class="col-md-12 text-center">
-                            <button type="submit" class="btn btn-primary btn-lg">Enviar</button>
+                            <button type="reset" class="btn btn-primary btn-lg" onClick="finalizarFormulario()">Enviar</button>
                         </div>
                     </div>
                 </fieldset>
@@ -53,3 +53,13 @@ let formu = $("#formulario").append(`<div class="container">
     </div>
 </div>
 </div>`);
+
+const finalizarFormulario = () => {
+  const formularioNombre = document.getElementById("nombreFormulario").value;
+  const formularioApellido =
+    document.getElementById("apellidoFormulario").value;
+  const Mensajeformulario = $("#formulario").append(
+    `<h3 class="modalFormulario">Muchas gracias por comunicarte con nosotros ${formularioNombre} ${formularioApellido} pronto nos pondremos en contacto contigo! </h3>`
+  );
+  Mensajeformulario();
+};
